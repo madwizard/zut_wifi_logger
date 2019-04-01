313628f40355500a6595ae22ac393593f1bc4e91
Damian Wojsław <damian@wojslaw.pl>
2019-04-01 19:35:57 +0200

    Splitting into multiple files

---
+++ gps.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+
+	"go.bug.st/serial.v1"
+)
+
+func InitGPS(portDevice string) (*serial.Port, []byte) {
+
+	mode := &serial.Mode{
+		BaudRate: 115200,
+		Parity:   serial.NoParity,
+		DataBits: 8,
+		StopBits: serial.OneStopBit,
+	}
+
+	port, err := serial.Open(portDevice, mode)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = port.Write([]byte("AT+CGNSPWR=1\r\nAT+CGNSSEQ=\"RMC\"\r\nAT+CGNSINF\r\nAT+CGNSURC=2\r\nAT+CGNSTST=1\r\n"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	buff := make([]byte, 400)
+
+	return &port, &buff
+}
--- main.go
+++ main.go
@@ -3,32 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
-
-	"go.bug.st/serial.v1"
 )
 
 func main() {
-
-	mode := &serial.Mode{
-		BaudRate: 115200,
-		Parity:   serial.NoParity,
-		DataBits: 8,
-		StopBits: serial.OneStopBit,
-	}
-
-	port, err := serial.Open("/dev/ttyUSB0", mode)
-	if err != nil {
-		log.Fatal(err)
-	}
+	port, buff := InitGPS("/dev/ttyUSB0")
 	defer port.Close()
 
-	_, err = port.Write([]byte("AT+CGNSPWR=1\r\nAT+CGNSSEQ=\"RMC\"\r\nAT+CGNSINF\r\nAT+CGNSURC=2\r\nAT+CGNSTST=1\r\n"))
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	buff := make([]byte, 400)
 	for {
 		n, err := port.Read(buff)
 		if err != nil {
