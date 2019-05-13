cfaabc39da8f1029761b6722a7220d4b95944b54
Damian Wojsław <damian@wojslaw.pl>
2019-05-13 20:29:55 +0200

    Add draft of db communication

---
+++ db.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func initDB() {
+	database, _ := sql.Open("sqlite3", "./wifidata.db")
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS wifidata (id INTEGER PRIMARY KEY, essid TEXT, mac TEXT)")
+	statement.Exec()
+}
+
+func writeDB(data wifiData) {
+	database, _ := sql.Open("sqlite3", "./wifidata.db")
+	statement, _ := database.Prepare("INSERT INTO wifidata (essid, mac) VALUES (?,?)")
+	statement.Exec(data.ESSID, data.MAC)
+}
+
+func readDB() *[]wifiData {
+	database, _ := sql.Open("sqlite3", "./wifidata.db")
+	rows, _ := database.Query("SELECT essid, mac from wifidata")
+	var data wifiData
+	var retdata []wifiData
+	{
+	}
+	for rows.Next() {
+		rows.Scan(&data.ESSID, &data.MAC)
+		retdata = append(retdata, data)
+	}
+
+	return &retdata
+}
--- wifi_test.go
+++ wifi_test.go
@@ -4,8 +4,14 @@ import (
 	"testing"
 )
 
+
+
+func TestReturnData(t *testing.T) {
+
+}
+
 func TestReadWiFiList(t *testing.T) {
-	out, err := readWiFiList("wlp4s0")
+	out, err := readList("wlp4s0")
 	if err != "" {
 		t.Errorf("readWiFiList returned error '%s'", err)
 	}
@@ -13,24 +19,3 @@ func TestReadWiFiList(t *testing.T) {
 		t.Errorf("readWiFiList returned empty list")
 	}
 }
-
-func TestGetKernelWEVersion(t *testing.T) {
-	out := getKernelWEVersion()
-	expected := 22
-	if out != expected {
-		t.Errorf("Expected '%d' but got '%d'", expected, out)
-	}
-}
-
-func TestIwSocketsOpen(t *testing.T) {
-	out := iwSocketsOpen()
-	if out < 0 {
-		t.Errorf("iwSocketsOpen returned value less than 0")
-	}
-}
-
-func TestIwSocketClose(t *testing.T) {
-	out := iwSocketsOpen()
-	iwSocketsClose(out)
-}
-
